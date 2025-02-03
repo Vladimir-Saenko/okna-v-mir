fc5ebacd829564607440bd7ae3701024a59a9f40
Vladimir Saenko <siborg.451@gmail.com>
2025-02-04 00:01:01 +0700

    пересборка

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <>
       <h2 className="font-[family-name:var(--font-oswald)]">
-        Какой-то контент
+        Сведения о компании и список контактов
       </h2>
     </>
   );
